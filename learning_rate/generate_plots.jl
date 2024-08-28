b04ab6a8e948ade4701a8977525c07e8d8ae7584
Wouter Nuijten <wouternuijten@gmail.com>
2024-08-28 14:27:34 +0200

    Make plots more robust

--- learning_rate/generate_plots.jl
+++ learning_rate/generate_plots.jl
@@ -10,13 +10,16 @@ minimum_so_far(vs) = [minimum(@view vs[1:i]) for i in 1:length(vs)]
 
 rxinfer_data = JSON.parsefile("rxinfer_results.json")
 
+stopping_threshold = 0.05
 
-p = plot(30:30:3000, rxinfer_data["Exponential"][2], yaxis=:log, label="Exponential", xlabel="Number of Neural Networks trained", ylabel="Variational Free Energy", legend=:topright, size=(350, 233))
-scatter!(p, [30 * 46], [rxinfer_data["Exponential"][2][46]], color="blue", primary=false, markershape=:x)
-plot!(p, 30:30:3000, rxinfer_data["Gamma"][2], label="Gamma")
-scatter!(p, [30 * 7], [rxinfer_data["Gamma"][2][7]], color=:red, primary=false, markershape=:x, markercolor=:red)
-plot!(p, 30:30:3000, rxinfer_data["InverseGamma"][2], label="Inverse Gamma")
-scatter!(p, [30 * 19], [rxinfer_data["InverseGamma"][2][19]], color=:green, primary=false, markershape=:x)
+index = [findfirst(x -> abs(x) < stopping_threshold, diff(rxinfer_data[dist][2])) for dist in keys(rxinfer_data)]
+
+p = plot(30 * (1:length(rxinfer_data["Exponential"][2])), rxinfer_data["Exponential"][2], yaxis=:log, label="Exponential", xlabel="Number of Neural Networks trained", ylabel="Variational Free Energy", legend=:topright, size=(350, 233))
+scatter!(p, [30 * index[1]], [rxinfer_data["Exponential"][2][index[1]]], color="blue", primary=false, markershape=:x)
+plot!(p, 30 * (1:length(rxinfer_data["Exponential"][2])), rxinfer_data["Gamma"][2], label="Gamma")
+scatter!(p, [30 * index[2]], [rxinfer_data["Gamma"][2][index[2]]], color=:red, primary=false, markershape=:x, markercolor=:red)
+plot!(p, 30 * (1:length(rxinfer_data["Exponential"][2])), rxinfer_data["InverseGamma"][2], label="Inverse Gamma")
+scatter!(p, [30 * index[3]], [rxinfer_data["InverseGamma"][2][index[3]]], color=:green, primary=false, markershape=:x)
 savefig(p, "rxinfer_results.tikz")
 
 bayesopt_results = JSON.parsefile("bayesopt_results.json")
